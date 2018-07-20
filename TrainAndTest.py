0d988f0681e17d05d0993a2554d5e7cc52868d9b
Ziv Cohen <6999501+ZivCohenZ@users.noreply.github.com>
2018-07-20 09:42:48 +0300

    Update TrainAndTest.py

--- TrainAndTest.py
+++ TrainAndTest.py
@@ -23,7 +23,6 @@ score2=model.TrainModel()
 mxgb=model.GetModel()
 
 score = rmsle_cv(mxgb,X,y,5,1)
-print("Xgboost score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
 
 
 
@@ -31,7 +30,6 @@ model2=xgb_model(train=X,y_train=y,random_state=122,num_rounds=3000,num_iter=10,
 model2.best_RMSE
 model.best_RMSE
 
-#Xgboost score: 0.1160 (0.0089)
 
 
 model3=xgb_model(train=X,y_train=y,random_state=122,num_rounds=3000,num_iter=10,eta=0.05,init_points=5)
@@ -40,7 +38,7 @@ model3.best_RMSE
 
 averaged_models = BaseStacking(models = (model2.GetModel(), model.GetModel(),model3.GetModel()))
 averaged_models
-#averaged_models = BaseStacking(models = (model2.GetModel(), model.GetModel()))
+
 
 score = rmsle_cv(averaged_models,X,y,5,15654)
-print(" Averaged base models score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
+
